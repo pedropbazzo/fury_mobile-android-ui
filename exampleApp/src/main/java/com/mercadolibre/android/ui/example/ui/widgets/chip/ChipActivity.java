97a03032435a0cb049fb07f8ecc89ac7cbbd06d9
Pablo <pablo.struminger@mercadolibre.com>
2019-11-14 10:50:54 -0300

    Add button to WidgetsActivity to open ChipActivity

--- exampleApp/src/main/java/com/mercadolibre/android/ui/example/ui/widgets/chip/ChipActivity.java
+++ exampleApp/src/main/java/com/mercadolibre/android/ui/example/ui/widgets/chip/ChipActivity.java
@@ -3,10 +3,18 @@ package com.mercadolibre.android.ui.example.ui.widgets.chip;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mercadolibre.android.ui.example.R;
+import com.mercadolibre.android.ui.widgets.MeliChip;
+
 public class ChipActivity extends AppCompatActivity {
+
+    private MeliChip meliChip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chip);
+
+        meliChip = findViewById(R.id.melichip);
     }
 }
