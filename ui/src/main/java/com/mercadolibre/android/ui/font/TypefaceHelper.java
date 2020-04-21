23bba17cfb8696f47ecf7fafed60396eae63b1ea
Martín Pérez <jose.martin@mercadolibre.com>
2020-04-21 10:17:11 -0300

    Migrate Font to andes

--- ui/src/main/java/com/mercadolibre/android/ui/font/TypefaceHelper.java
+++ ui/src/main/java/com/mercadolibre/android/ui/font/TypefaceHelper.java
@@ -8,20 +8,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.res.ResourcesCompat;
 import android.widget.TextView;
 
-import static android.support.v4.provider.FontsContractCompat.FontRequestCallback.FAIL_REASON_FONT_NOT_FOUND;
-
 /**
  * This class is used as a wrapper for our custom font.
  * If you code create a View that supports typeface you should call one of this methods.
  */
 public final class TypefaceHelper {
-
-    @NonNull
-    private static TypefaceSetter typefaceSetter;
-
-    static {
-        typefaceSetter = new CalligraphyTypefaceSetter();
-    }
 
     private TypefaceHelper() {
         //Do nothing, this class should not be instantiated
@@ -32,7 +23,7 @@ public final class TypefaceHelper {
      * @param typefaceSetter field
      */
     public static void attachTypefaceSetter(@NonNull final TypefaceSetter typefaceSetter) {
-        TypefaceHelper.typefaceSetter = typefaceSetter;
+        com.mercadolibre.android.andesui.font.TypefaceHelper.Companion.attachTypefaceSetter(typefaceSetter);
     }
 
     /**
@@ -42,7 +33,9 @@ public final class TypefaceHelper {
      * @param font  The {@link Font} the text should have
      */
     public static <T extends TextView> void setTypeface(@NonNull final T view, @NonNull final Font font) {
-        typefaceSetter.setTypeface(view, font);
+        com.mercadolibre.android.andesui.font.TypefaceHelper.Companion.setTypeface(
+                view, com.mercadolibre.android.andesui.font.Font.valueOf(font.name())
+        );
     }
 
     /**
@@ -53,7 +46,9 @@ public final class TypefaceHelper {
      * @param font    The {@link Font} the text should have
      */
     public static void setTypeface(@NonNull final Context context, @NonNull final Paint paint, @NonNull final Font font) {
-        typefaceSetter.setTypeface(context, paint, font);
+        com.mercadolibre.android.andesui.font.TypefaceHelper.Companion.setTypeface(
+                context, paint, com.mercadolibre.android.andesui.font.Font.valueOf(font.name())
+        );
     }
 
     /**
@@ -69,12 +64,9 @@ public final class TypefaceHelper {
     @SuppressWarnings("PMD.LinguisticNaming")
     @Deprecated
     public static void getTypeface(@NonNull final Context context, @NonNull final Font font, @NonNull final ResourcesCompat.FontCallback fontCallback) {
-        Typeface typeface = TypefaceHelper.getFontTypeface(context, font);
-        if (typeface == null) {
-            fontCallback.onFontRetrievalFailed(FAIL_REASON_FONT_NOT_FOUND);
-        } else {
-            fontCallback.onFontRetrieved(typeface);
-        }
+        com.mercadolibre.android.andesui.font.TypefaceHelper.Companion.getTypeface(
+                context, com.mercadolibre.android.andesui.font.Font.valueOf(font.name()), fontCallback
+        );
     }
 
     /**
@@ -86,37 +78,14 @@ public final class TypefaceHelper {
      */
     @Nullable
     public static Typeface getFontTypeface(@NonNull final Context context, @NonNull Font font) {
-        return typefaceSetter.getTypeface(context, font);
+        return com.mercadolibre.android.andesui.font.TypefaceHelper.Companion.getFontTypeface(
+                context, com.mercadolibre.android.andesui.font.Font.valueOf(font.name())
+        );
     }
 
     /**
      * Setter for typeface
      */
-    public interface TypefaceSetter {
+    public interface TypefaceSetter extends com.mercadolibre.android.andesui.font.TypefaceHelper.TypefaceSetter {}
 
-        /**
-         * Set a typeface to a view
-         * @param view to set the typeface to
-         * @param font to set
-         * @param <T> extends TextView
-         */
-        <T extends TextView> void setTypeface(@NonNull final T view, @NonNull final Font font);
-
-        /**
-         * Set a typeface to the paint
-         * @param context to use
-         * @param paint to set the typeface
-         * @param font to set
-         */
-        void setTypeface(@NonNull final Context context, @NonNull final Paint paint, @NonNull final Font font);
-
-        /**
-         * Return the typeface associated with the font
-         * @param context to use
-         * @param font to find the typeface
-         * @return typeface associated
-         */
-        @Nullable
-        Typeface getTypeface(@NonNull Context context, @NonNull final Font font);
-    }
 }
